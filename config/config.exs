c63ed553a2faa384486e495edbd2a3b42d7e7345
alsdiufgoaiwuegflweuvflasjkdhvlajhsdfg666272727asfgfdsagdlsafg <aslfdjgqwoeuflsad@sadufgalksdjfbqwlekuvgasldiuvgasdfvalkwejbra>
2018-11-18 21:31:51 +0000

    start sasl before logger

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,8 @@ use Mix.Config
 
 config :kernel, inet_dist_use_interface: {127, 0, 0, 1}
 
+config :logger,
+  handle_otp_reports: true,
+  handle_sasl_reports: true
+
 import_config "#{Mix.env()}.exs"
